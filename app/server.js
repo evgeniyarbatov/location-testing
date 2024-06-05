72ee7148cda2902501a1ce40824028f7e2e1298b
Evgeny Arbatov <arbatovevgeny@gmail.com>
2024-06-05 15:10:05 +0800

    Server to return location

---
+++ app/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+const port = 80;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/location', (req, res) => {
+    const { latitude, longitude } = req.query;
+    res.send(`${latitude} ${longitude}`);
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
